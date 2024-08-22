24585f6ee4fa919378d0c65262598fc9147f9aa7
Adeonir Kohl <adeonir@gmail.com>
2024-08-22 18:27:47 -0300

    Create category card component

---
+++ src/components/main-content/CategoryCard.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import type { PropType } from 'vue'
+
+import type { Category } from '@/types/Category'
+
+export default {
+  name: 'CategoryCard',
+  props: {
+    category: { type: Object as PropType<Category>, required: true }
+  }
+}
+</script>
+
+<template>
+  <li class="category-card">
+    <h2 class="heading-md category-title">{{ category.name }}</h2>
+    <ul class="ingredients-list">
+      <li class="ingredient-card">{{ category.image }}</li>
+    </ul>
+  </li>
+</template>
