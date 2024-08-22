7faadf6f2e26b1a49eb13bac61452d9d75aff666
Adeonir Kohl <adeonir@gmail.com>
2024-08-22 19:05:18 -0300

    Update category card

--- src/components/main-content/CategoryCard.vue
+++ src/components/main-content/CategoryCard.vue
@@ -17,10 +17,55 @@ export default {
 </script>
 
 <template>
-  <li class="category-card">
-    <h2 class="heading-md category-title">{{ category.name }}</h2>
+  <article class="category-card">
+    <header class="card-header">
+      <img :src="`/images/icons/categories/${category.image}`" :alt="category.name" class="card-image" />
+      <h2 class="paragraph-lg category-title">{{ category.name }}</h2>
+    </header>
     <ul class="ingredients-list">
-      <li class="ingredient-card">{{ category.image }}</li>
+      <li v-for="ingredient in category.ingredients" :key="ingredient" class="ingredient-card">
+        <IngredientTag :name="ingredient" />
+      </li>
     </ul>
-  </li>
+  </article>
 </template>
+
+<style scoped>
+.category-card {
+  width: 19.5rem;
+  padding: 1rem 1.5rem 1.5rem;
+  border-radius: 1rem;
+  background: var(--white);
+  box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.card-header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.card-image {
+  width: 5rem;
+}
+
+.category-title {
+  text-align: center;
+  color: var(--green);
+  font-weight: 700;
+}
+
+.ingredients-list {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+}
+</style>
