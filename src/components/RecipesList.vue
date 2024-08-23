0d952b27824dd8e7f7f50e2295981a99cd9e620c
Adeonir Kohl <adeonir@gmail.com>
2024-08-23 15:22:29 -0300

    Fetch recipes and set the component state

--- src/components/RecipesList.vue
+++ src/components/RecipesList.vue
@@ -1,6 +1,31 @@
 <script lang="ts">
+import MainButton from '@/components/MainButton.vue'
+import RecipeCard from '@/components/RecipeCard.vue'
+import type { Recipe } from '@/types/Recipe'
+
 export default {
-  name: 'RecipesList'
+  name: 'RecipesList',
+  components: {
+    RecipeCard,
+    MainButton
+  },
+  methods: {
+    async fetchRecipes() {
+      const response = await fetch(
+        'https://gist.githubusercontent.com/adeonir/aee35a83ba43245c4ec5edd1cc8e1827/raw/c219a3178191efd334dbbfe4c5e90c03ed7801f5/recipes.json'
+      )
+      const recipes = await response.json()
+      return recipes
+    }
+  },
+  async created() {
+    this.recipes = await this.fetchRecipes()
+  },
+  data() {
+    return {
+      recipes: [] as Array<Recipe>
+    }
+  }
 }
 </script>
 
@@ -10,6 +35,10 @@ export default {
     <p class="paragraph-lg subtitle">
       Veja as opções de receitas que encontramos com os ingredientes que você tem por aí:
     </p>
+    <ul class="recipes-list">
+      <RecipeCard v-for="recipe in recipes" :key="recipe.name" :recipe="recipe" />
+    </ul>
+    <MainButton label="Buscar receitas" @click="$emit('search-recipes')" />
   </section>
 </template>
 
@@ -29,5 +58,14 @@ section {
 
 .subtitle {
   margin-bottom: 2rem;
+}
+
+.recipes-list {
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  flex-wrap: wrap;
+  margin-bottom: 3.5rem;
 }
 </style>
