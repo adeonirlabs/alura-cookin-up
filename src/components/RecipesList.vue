793f90bcd0432e54e2cd9c63f65e9fecae013e26
Adeonir Kohl <adeonir@gmail.com>
2024-08-23 08:16:02 -0300

    Create recipes list component

---
+++ src/components/RecipesList.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+export default {
+  name: 'RecipesList'
+}
+</script>
+
+<template>
+  <section>
+    <h1 class="heading-lg title">Receitas</h1>
+    <p class="paragraph-lg subtitle">
+      Veja as opções de receitas que encontramos com os ingredientes que você tem por aí:
+    </p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.title {
+  color: var(--green);
+  display: block;
+  margin-bottom: 1.5rem;
+}
+
+.subtitle {
+  margin-bottom: 2rem;
+}
+</style>
