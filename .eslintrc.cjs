631179091dbb0399ab848b66bf7e23ddf6e82e7f
Adeonir Kohl <adeonir@gmail.com>
2024-08-23 23:18:53 -0300

    Update lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'prettier',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   plugins: ['import', 'simple-import-sort', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'import/default': 'off',
     'import/export': 'off',
     'import/first': 'error',
@@ -30,6 +31,6 @@ module.exports = {
     'no-var': 'error',
     'object-shorthand': 'warn',
     'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
+    'simple-import-sort/exports': 'error'
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
     port: 3000
   },
